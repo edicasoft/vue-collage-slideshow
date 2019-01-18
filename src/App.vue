348f001d89d9bfce77b5fd87e4d4689f2cb4aad6
Daria Mykhailova <daria.edicasoft@gmail.com>
2019-01-18 16:31:14 +0200

    remove old app

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,109 @@
 <template>
     <div id="app">
         <transition name="fade">
-            <h1 v-if="noImages && !isLoading">No Images</h1>
+            <h1 v-if="noImages  && !isLoading">No Images</h1>
         </transition>
         <loading-spinner :delay="1500" :loader="isLoading" class="center" text="Loading Images"></loading-spinner>
-        <transition name="fade">
-            <carousel v-if="!noImages && !isLoading" :per-page="1" :paginationEnabled="false" :autoplay="true"
-                      :autoplayTimeout="3000"
-                      :autoplayHoverPause="false">
-                <slide v-for="(slide, idx) in slides" :key="idx">
-                    <component :is="slideTemplate(slide)" :class="slideClass(slide)" :images="slide"/>
-                </slide>
-            </carousel>
-        </transition>
+        <component :is="slideTemplate(slide.length)"
+                   v-for="(slide, idx) in slides"
+                   :animationDuration="animationDuration"
+                   :slidesInterval="slidesInterval"
+                   :key="idx"
+                   v-if="activeSlide == idx"
+                   :status="status"
+                   :images="slide"/>
     </div>
 </template>
 
 <script>
-    import InnerSlide from './InnerSlide';
-    import TripleSlide from './TripleSlide';
+    import SimpleSlide from '@/components/slides/SimpleSlide';
+    import TripleSlide from '@/components/slides/TripleSlide';
+    import FourImagesSlide from '@/components/slides/FourImagesSlide';
+    import FiveImagesSlide from '@/components/slides/FiveImagesSlide';
     import LoadingSpinner from '@/components/LoadingSpinner';
     export default {
         name: 'app',
         components: {
-            InnerSlide, TripleSlide, LoadingSpinner
+            SimpleSlide, TripleSlide, FourImagesSlide, LoadingSpinner, FiveImagesSlide
         },
         props:['images'],
         data(){
             return {
                 slides: [],
-                isLoading: false
+                isLoading: false,
+                status: 0, //  0 = idle, 1 = running, 2 = paused, 3 = resumed
+                slidesTimeout: false,
+                activeSlide: 0,
+                slidesInterval: 4000,
+                animationDuration: 500,
+                animationTimeout: false,
+//                images: [
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop-1024x683.jpg"},
+//
+//                    {image: "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png"},
+//                    {image: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//
+//                    {image: "https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg"},
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop-1024x683.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/802px-Eiffel_Tower_Vertical.JPG"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg"},
+//                    {image: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"},
+//                    {image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Eiffel_Tower_Vertical.JPG/2048px-Eiffel_Tower_Vertical.JPG"},
+//
+//                ]
             }
         },
         computed: {
@@ -42,45 +116,81 @@
             this.isLoading = true;
             this.loadImages()
                     .then(values => {
-                        this.createCollages(values);
+                        this.createCollages(values.filter(item => !item.is_error));
                     })
                     .catch(()=> {
 //                       console.log(er);
                     })
                     .finally(()=> {
                         this.isLoading = false;
-
+                        this.play();
+                        window.addEventListener('keyup', this.pressSpacebar)
                     });
         },
         methods: {
-            slideTemplate(slide){
-                return slide.length == 3 ? 'TripleSlide' : 'InnerSlide'
+            pressSpacebar(e){
+                if (e.keyCode == 32) {
+//                    console.log(this.status);
+                    switch (this.status) {
+                        case 1:
+                        case 3:
+                            this.pause();
+                            break;
+                        case 2:
+                            this.resume();
+                            break;
+                        default:
+                            this.play()
+                    }
+                }
             },
-            slideClass(slide){
-                let slideClass = {};
-                if (slide.length == 3) slideClass['is-triple'] = true;
-                else if (slide.length == 2) slideClass['is-double'] = true;
-                else slideClass['is-single'] = true;
-                const verticals = slide.filter(item => item.isVertical);
-                const horizontals = slide.filter(item => item.isHorizontal);
-                if (verticals.length == 2 && slideClass['is-double']) slideClass['of-verticals'] = true;
-                if (horizontals.length == 2 && slideClass['is-double']) slideClass['of-horizontals'] = true;
-                return slideClass
+            pause(){
+//                console.log('pause');
+                clearTimeout(this.slidesTimeout);
+                this.status = 2;
             },
-            loadImage(src){
+            resume(){
+//                console.log('resume');
+                this.status = 3;
+                clearTimeout(this.animationTimeout);
+                this.animationTimeout = setTimeout(this.nextSlide, this.animationDuration);
+            },
+            play(){
+                if (this.slides.length <= 0) return;
+//                console.log('play');
+                this.startSlidesTimeout();
+            },
+            nextSlide(){
+                this.activeSlide++;
+                if (this.activeSlide >= this.slides.length) {
+                    this.activeSlide = 0;
+                }
+//               console.log('slide', this.activeSlide);
+                this.startSlidesTimeout();
+            },
+            startSlidesTimeout(){
+                this.status = 1;
+                clearTimeout(this.slidesTimeout);
+                this.slidesTimeout = setTimeout(()=> {
+                    this.nextSlide();
+                }, this.slidesInterval);
+            },
+            //TODO::remove idx
+            loadImage(src, idx){
                 return new Promise(function (resolve) {
                     let img = new Image();
                     img.onload = function () {
                         const isHorizontal = this.width >= this.height;
                         resolve({
-                            src: src,
+//                            idx,
+                            image: src,
                             isHorizontal: isHorizontal,
                             isVertical: !isHorizontal,
                         });
                     };
                     img.onerror = function () {
                         resolve({
-                            src: src,
+                            image: src,
                             is_error: true
                         });
                     };
@@ -88,54 +198,40 @@
                 });
             },
             loadImages(){
-                this.slides = [];
                 var promises = [];
 
-                this.images.forEach(item => {
-                    promises.push(this.loadImage(item.image));
+                this.images.forEach((item, idx)=> {
+                    promises.push(this.loadImage(item.image, idx));
                 });
                 return Promise.all(promises);
             },
-
-            slideIsFull(slide, next_image){
-                let isFull = false;
-                if (slide.length == 3) {
-                    return true;
+            slideTemplate(count){
+                switch (count) {
+                    case 3:
+                        return 'TripleSlide';
+                    case 4:
+                        return 'FourImagesSlide';
+                    case 5:
+                        return 'FiveImagesSlide';
+                    default:
+                        return 'SimpleSlide';
                 }
-                const horizontals = slide.filter(item => item.isHorizontal);
-                const verticals = slide.filter(item => item.isVertical);
-
-                if (horizontals.length == 2 && next_image.isHorizontal) isFull = true;
-                else if (verticals.length == 2) isFull = true;
-                else if (horizontals.length == 1 && verticals.length == 1 && next_image.isVertical) isFull = true;
-
-                return isFull;
             },
-            setFirstVertical(slide){
-                slide.sort((x, y) => x.isVertical ? -1 : y.isVertical ? 1 : 0);
-                return slide;
-            },
-            pushSlide(slide){
-                slide = this.setFirstVertical(slide);
-                this.slides.push(slide);
+            getRandomInt(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min;
             },
             createCollages(images){
-//                console.log(images);
-                let slide = [];
                 this.slides = [];
-                images.forEach((image) => {
-                    if (image.is_error) return;
-                    if (this.slideIsFull(slide, image)) {
-//                        console.log('slideIsFull');
-                        slide = this.setFirstVertical(slide);
-                        this.pushSlide(slide);
-                        slide = [];
-                    }
-//                    console.log(i, image.isHorizontal ? 'horizontal' : 'vertical');
-                    slide.push(image);
-                });
-                this.pushSlide(slide);
-//                console.log('slides', this.slides);
+                let index = 0;
+                let size = 1;
+                while (index < images.length) {
+                    size = this.getRandomInt(2, 5);
+                    let slide = images.slice(index, size + index);
+                    this.slides.push(slide);
+                    index = size + index;
+                }
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './DevApp.vue'
+import App from './App.vue'
 import '@/assets/main.scss'
 import '@/assets/slides.scss'
 import '@/assets/animation.scss'
